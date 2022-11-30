a68813a0eb96d269323459af585397eae8bda2af
Mario Nachbaur <marionauta@users.noreply.github.com>
2022-11-30 13:23:08 +0100

    feat: add fountain properties to domain

--- app/src/main/java/mn/fountains/domain/models/Fountain.kt
+++ app/src/main/java/mn/fountains/domain/models/Fountain.kt
@@ -6,10 +6,12 @@ data class Fountain(
     val id: String,
     val name: String,
     val location: Location,
+    val properties: FountainProperties,
 )
 
 fun FountainDto.intoDomain(): Fountain = Fountain(
     id = id,
     name = name,
     location = location.intoDomain(),
+    properties = properties.intoDomain(),
 )
