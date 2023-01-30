875215ace9eec490b04408ef0fb0202a5d978f03
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-01-30 20:27:56 +0100

    feat: tweak colors

--- app/src/main/java/mn/fountains/ui/theme/Color.kt
+++ app/src/main/java/mn/fountains/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package mn.fountains.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val ColorPrimary = Color(0xFF264653)
+val ColorSecondary = Color(0xFFf4a261)
+val ColorSecondaryDark = Color(0xFFE76F51)
