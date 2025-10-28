4f6c9231909dad006700bc5e3086c6e7fcd74dc2
Mario Nachbaur <marionauta@users.noreply.github.com>
2025-10-28 16:45:30 +0100

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "mn.openlocations"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 24
-        versionName = "2.2.0"
+        versionCode = 26
+        versionName = "2.3.0"
     }
     packaging {
         resources {
